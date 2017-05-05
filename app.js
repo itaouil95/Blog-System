523212dcac415e2e75718e4234f135e12f4c479e
Ilyass Taouil <itaouil95@gmail.com>
2017-05-05 18:55:49 +0200

    Added a basic layout, plus an image found on the web. Ready for node.

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+// Modules
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var flash        = require('connect-flash');
+var validator    = require('express-validator');
+var session      = require('express-session');
+var emessages    = require('express-messages');
+var moment       = require('moment');
+var mongo        = require('mongodb');
+var db           = require('monk')('localhost/nodeblog');
+var multer       = require('multer');
+var upload       = multer({ dest: './uploads/' })
 
+// Routes module
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// Express instance
 var app = express();
 
-// view engine setup
+// View's rendering engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middlewares
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,6 +35,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Express session middleware
+app.use(session({
+  secret: 'I am not a secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// Flash middleware
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express validator middleware
+app.use(validator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Make db (via monk ORM) accessible to router
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
+});
+
+// Routes middleware
 app.use('/', index);
 app.use('/users', users);
 
